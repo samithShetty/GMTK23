04442ca2af6e0905e920133a49e76e70001b37a9
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2023-07-07 15:13:43 -0400

    Implemented Square + Circle blocks and player controller

---
+++ player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+@export var blocks : Array[PackedScene]
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("build"): # Place Random Block 
+		var block = blocks.pick_random().instantiate()
+		block.position = get_global_mouse_position()
+		add_sibling(block)
+		print(get_global_mouse_position() - position)
